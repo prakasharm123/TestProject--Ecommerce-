eb0e1702ac01691568babe13c53a5d4a489711c2
prakash <sharmaprakash1234321@gmail.com>
2025-08-06 11:12:26 +0530

    making changes

--- src/test/java/Prakash/tests/Test.java
+++ src/test/java/Prakash/tests/Test.java
@@ -13,6 +13,7 @@ public class Test {
 		System.out.println("i am prakash ");
 		System.out.println("i am studying automation");
 		System.out.println("this commit from Test project Ecommerce");
+		System.out.println("i am making some changes in project");
 		
 		
 		
