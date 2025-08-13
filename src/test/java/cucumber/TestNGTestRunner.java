84e31524aa7c3f07847a2fad6d1c62ed9cfeb3ec
prakash <sharmaprakash1234321@gmail.com>
2025-08-13 21:51:48 +0530

    TestngTestRunner class update

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/cucumber",glue = "stepDefinations",
-monochrome = true,tags = "@ErrorValidation", plugin = {"html:target/cucumber.html"})
+monochrome = true,tags = "@Regression", plugin = {"html:target/cucumber.html"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 	
 
